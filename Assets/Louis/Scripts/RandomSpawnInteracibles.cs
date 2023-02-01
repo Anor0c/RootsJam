e6e3d02457a39b11bdbc0fb560cf9e2a53daf262
Anor0c <94901070+Anor0c@users.noreply.github.com>
2023-02-01 12:30:21 +0100

    Create interactible spawner

---
+++ Assets/Louis/Scripts/RandomSpawnInteracibles.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnInteracibles : MonoBehaviour
+{
+    void Start()
+    {
+     /*logique pour faire spawn les objtes de maniere random dans une strate 
+      il faudrait du coup définir une strate= public gameobject strate?
+     pb: comment je lui definie l'aire de ma strate, si les frontieres sont random?
+
+     puis algo pour random spawn dans la strate= je sais pas mais je me soigne*
+
+     algo = simple random walk? t'a un agent qui se balade dans la strate dans les 4 dir cardinales et tous les x deplacement il spawn un interactible?
+
+     et la normalement on est gucci*/   
+    }
+}

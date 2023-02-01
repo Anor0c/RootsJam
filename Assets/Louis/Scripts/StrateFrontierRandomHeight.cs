e0b756ddd21e78bd8758f96e7ed064de815f121b
Anor0c <94901070+Anor0c@users.noreply.github.com>
2023-02-01 12:32:06 +0100

    Update StrateFrontierRandomHeight.cs

--- Assets/Louis/Scripts/StrateFrontierRandomHeight.cs
+++ Assets/Louis/Scripts/StrateFrontierRandomHeight.cs
@@ -6,6 +6,13 @@ public class StrateFrontierRandomHeight : MonoBehaviour
 {
     void Start()
     {
-        
+        /*Définir des points sur le coté bas de la strate 1, chaque point equidistant du point avant et apres 
+         dire au points de se placer : for each point in pointarray
+        pos.y =random.range(hauteur minimm,hauteur maximum maximum)
+        relier les points 
+        dire que la forme créée est un masque dans lequel on peut voir la texture de la strate 1
+
+        faire de meme pour les strate 2 et 3? pas sur, ca parait pas opti
+         */
     }
 }

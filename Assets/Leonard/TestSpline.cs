8c9d6110717034b63ff7db6c835a7ff07e72766e
leonardV32 <83813949+leonardV32@users.noreply.github.com>
2023-02-02 15:09:32 +0100

    Click to start and prite shape following head

---
+++ Assets/Leonard/TestSpline.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.U2D;
+using UnityEngine;
+
+public class TestSpline : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Spline targetSpline;
+    void Start()
+    {
+    // Spline targetSpline = GetComponentInParent()
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //targetSpline.
+        //gnération
+
+    }
+}
--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 using UnityEngine.InputSystem;
 
 public class MovementPlayer : MonoBehaviour
@@ -12,35 +13,43 @@ public class MovementPlayer : MonoBehaviour
     Vector2 currentVelocityV2;
     float currentVelocity;
     Vector2 direction;
-    
+
+    bool isMoving = false;
+
     [SerializeField] float maxTurnSpeed;
     float angle;
+
+    private Spline RootBody;
+
     // Start is called before the first frame update
     void Start()
     {
         UpdateMoveSpeed();
+        var SplineController = FindObjectOfType<SpriteShapeController>();
+        RootBody = SplineController.spline;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        
         //Get Mouse Position
         mousePosition = Mouse.current.position.ReadValue();
-        Debug.Log(mousePosition);
+        //Debug.Log(mousePosition);
         var mousePosition3 = new Vector3(mousePosition.x, mousePosition.y, 10);
         var worldpos = Camera.main.ScreenToWorldPoint(mousePosition3);
-        
         //direction
         direction = new Vector2(worldpos.x - transform.position.x, worldpos.y - transform.position.y);
-        
-
-
         //RB2D move and rotate
         float angle = Vector2.SignedAngle(Vector2.right, direction);
         Vector3 targetRotation = new Vector3(0, 0, angle);
         rb2D.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed * Time.deltaTime));
-        rb2D.MovePosition(rb2D.position + ((Vector2)transform.right * moveSpeed * Time.deltaTime));
+        if (isMoving == true)
+            rb2D.velocity = transform.right * moveSpeed;
+            RootBody.SetPosition(RootBody.GetPointCount() - 1, rb2D.position);
 
+
+        //rb2D.MovePosition(rb2D.position + ((Vector2)transform.right * moveSpeed * Time.deltaTime));
         // rotate sprite towards mouse
         //float angle = Vector2.SignedAngle(Vector2.right, direction);
         //Vector3 targetRotation = new Vector3(0, 0, angle);
@@ -57,13 +66,9 @@ public class MovementPlayer : MonoBehaviour
     {
         moveSpeed = Mathf.Sqrt(moveSpeed*3);
     }
-    public void Steer()
-    {   /*
-        screenPosition = Mouse.current.position.ReadValue();
-        screenPosition.z = Camera.main.nearClipPlane + 1;
-        WorldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-        transform.position = WorldPosition;
-        */
 
+    public void StartMoving()
+    {
+        isMoving = true;
     }
 }
